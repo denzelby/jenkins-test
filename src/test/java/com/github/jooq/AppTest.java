d400350765031693e7aa0415bcbff3b96f4bebd7
Andrei.Hubarevich <andrei.hubarevich@synesis.ru>
2018-05-15 15:47:23 +0300

    add test

---
+++ src/test/java/com/github/jooq/AppTest.java
@@ -0,0 +1,13 @@
+package com.github.jooq;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertEquals(2, 2);
+    }
+}
